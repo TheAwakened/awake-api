f0378348b9dfee41e80ec8d058727c23a81eef2f
kw7oe <choongkwern@hotmail.com>
2017-12-18 19:10:51 +0800

    Add api_only to application.rb, Remove unused gems

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Awake
   class Application < Rails::Application
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.autoload_paths << "#{Rails.root}/lib"
