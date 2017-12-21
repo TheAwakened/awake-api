438f5e037c8dc0c88e7b5b22558bdbf91e0dcf9a
kw7oe <choongkwern@hotmail.com>
2017-12-21 13:28:19 +0800

    Limit creating awakening record for user to once per day

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :awakenings
   has_secure_password
   validates :username, uniqueness: true
+
+  def awake_today?
+    awakenings.today.present?
+  end
 end
 
 
